2736f86cd9659c6239bed4c64f14c3a72595fa6f
Jer Wilson <jwilson@classifiedads.com>
2015-10-20 02:48:31 -0700

    kinda lame

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import sys
-from collections import namedtuple
+import random
 
 hyrule = """\
                                                                  \
@@ -61,13 +61,25 @@ colors = [
     0x1d,0x18,0x13,0x15,0x39,0x5d,0x81,0xa5,0xc8,0xcd,0xd3
 ]
 
-needs = 'wfrlabm'
-items = 'WFRLABM'
+needbits = 'wfrlabm'
+itembits = 'WFRLABM'
 nodes = []
 start_nr = 0
-pois = []
+poi_list = []
 warps = [0] * 9
 roads = []
+
+# changey things
+pois = []
+lvls = None
+items = 0
+hearts = 3
+#budget = 200
+#wallet = 10
+lunk = 0
+sequence = None
+distance = 0
+report = ""
 
 class Node:
     lvl=0
@@ -88,24 +100,13 @@ class Node:
     def __init__(self):
         self.links = []
 
-    def __str__(self):
-        return ' '.join([
-            str(self.lvl),
-            str(self.need),
-            str(self.item),
-            str(self.money),
-        ])
-
 class State:
     visited = None
     backlinks = None
-    items = 0
-    lvls = None
 
     def __init__(self):
         self.visited = [9999] * 128
         self.backlinks = [False] * 128
-        self.lvls = []
 
 for y in range(0,8):
     for x in range(0,16):
@@ -124,10 +125,11 @@ for y in range(0,8):
             if f >= '1' and f <= '8':
                 node.lvl = int(f)
                 node.money = level_money[node.lvl]
-            elif f in needs:
-                node.need |= 2 ** needs.index(f)
-            elif f in items:
-                node.item |= 2 ** items.index(f)
+                warps[node.lvl] = node_nr
+            elif f in needbits:
+                node.need |= 2 ** needbits.index(f)
+            elif f in itembits:
+                node.item |= 2 ** itembits.index(f)
             elif f == 'X':
                 node.money = 10
             elif f == 'T':
@@ -160,40 +162,52 @@ for y in range(0,8):
         if right in '_>' or node.scrr:
             node.links.append(node_nr + 1)
 
-        if node.lvl > 0 or node.item > 0 or node.heart or node.money > 0:
-            pois.append(node_nr)
+        if node.lvl > 0 or node.item > 0 or node.heart:
+            poi_list.append(node_nr)
 
         nodes.append(node)
 
+def node2chr(node):
+    if node.lvl > 0:
+        return str(node.lvl)
+    elif node.item & 2:
+        return 'f'
+    elif node.item & 16:
+        return 'a'
+    elif node.item & 64:
+        return 'm'
+    elif node.money == 10:
+        return 'X'
+    elif node.money == 30:
+        return 'T'
+    elif node.money == 100:
+        return 'C'
+    elif node.heart:
+        return 'H'
+    elif node.road:
+        return '+'
+    else:
+        return '.'
+
 def statout(state, path=[]):
+    out = ""
     for i, node in enumerate(nodes):
         if i not in path:
-            sys.stdout.write("\x1B[38;5;236m")
+            out += "\x1B[38;5;236m"
         elif state.visited[i] < 9999:
-            sys.stdout.write("\x1B[38;5;" + str(colors[state.visited[i]]) + "m")
+            out += "\x1B[38;5;" + str(colors[state.visited[i]]) + "m"
         else:
-            sys.stdout.write("\x1B[0m")
+            out += "\x1B[0m"
 
-        if node.lvl > 0:
-            sys.stdout.write(str(node.lvl))
-        elif node.item & 2:
-            sys.stdout.write('f')
-        elif node.item & 16:
-            sys.stdout.write('a')
-        elif node.item & 64:
-            sys.stdout.write('m')
-        elif node.heart:
-            sys.stdout.write('H')
-        elif node.road:
-            sys.stdout.write('+')
-        elif i == start_nr:
-            sys.stdout.write('$')
+        if i == start_nr:
+            out += "$"
         else:
-            sys.stdout.write('.')
+            out += node2chr(node)
 
         if i % 16 == 15:
-            sys.stdout.write('\n')
-    sys.stdout.write("[0m\n")
+            out += "\n"
+    out += "[0m"
+    return out
 
 def flood(state, cur, prev, step):
     if state.visited[cur] <= step:
@@ -204,16 +218,110 @@ def flood(state, cur, prev, step):
     for x in nodes[cur].links:
         flood(state, x, cur, step+1)
 
+    if step == 0 and items & 1: # whistle warps
+        for x in lvls:
+            flood(state, warps[x], cur, step+1)
+
 def shortest(state, cur):
     out = []
     while state.visited[cur] > 0:
         out.append(cur)
         cur = state.backlinks[cur]
     return out
-    
-state = State()
-flood(state, start_nr, 0, 0)
-path = shortest(state, pois[3])
-print path
 
-statout(state, path)
+best = 9999
+
+while True:
+    pois[:] = poi_list
+    lvls = []
+    items = 0
+    hearts = 3
+    #budget = 200
+    #wallet = 10
+    lunk = start_nr
+    sequence = []
+    distance = 0
+    report = ""
+
+    while len(lvls) < 8:
+        # choose a destination
+        random.shuffle(pois)
+        found = False
+        for rot in range(0, len(pois)):
+            node = nodes[pois[-1]]
+
+            # wfrlabm
+            if node.lvl == 0 and node.item & 2:
+                cost = 60
+            elif node.item & 16:
+                cost = 80
+            elif node.item & 64:
+                cost = 60 if node.discount else 100
+            else:
+                cost = 0
+
+            # if node.lvl == 0 and node.money > 0 and wallet >= budget:
+            #     # too much money
+            #     pass
+            # if cost > wallet:
+            #     # not enough money
+            #     pass
+            if node.heart and hearts >= 12:
+                # too many hearts
+                pass
+            elif node.lvl in [6,8] and hearts < 12:
+                # not enough hearts
+                pass
+            elif node.lvl == 0 and node.item > 0 and node.item & items == node.item:
+                # already have this item
+                pass
+            elif node.need & items == node.need:
+                found = True
+                break
+            else:
+                # print 'missing item:', node.need & ~items
+                pass
+            pois[:] = pois[1:] + pois[:1]
+
+        if not found:
+            print "error"
+            print "lvls:", lvls
+            print "pois:", pois
+            print "items:", items
+            sys.exit(0)
+
+        node_nr = pois.pop()
+
+        # wallet -= cost
+        # budget -= cost
+        # if node.lvl == 7 and items & 2 == 0: # don't have candle already
+        #     budget -= 60
+
+        items |= node.item
+
+        if node.heart:
+            hearts += 1
+
+        # wallet += node.money
+
+        state = State()
+        flood(state, lunk, 0, 0)
+        path = shortest(state, node_nr)
+        distance += len(path)
+        seqpoint = [node_nr, node2chr(node)]
+        sequence.append(seqpoint)
+        lunk = node_nr
+        report += statout(state, path)
+
+        if node.lvl > 0:
+            lvls.append(node.lvl)
+            hearts += 1
+
+    report += "levels: " + str(lvls) + "\n"
+    report += "hearts: " + str(hearts) + "\n"
+    report += "sequence: " + str(sequence) + "\n"
+    report += "distance: " + str(distance) + "\n"
+
+    if distance < best:
+        best = distance
+        print report
